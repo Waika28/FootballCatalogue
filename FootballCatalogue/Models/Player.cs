33f93cae85695e8a31fbbd05831373b913b1efd2
Евгений Рыбин <z.ribin20@gmail.com>
2023-01-20 18:54:13 +0500

    Fix: add id to `Player`

--- FootballCatalogue/Models/Player.cs
+++ FootballCatalogue/Models/Player.cs
@@ -4,6 +4,7 @@ namespace FootballCatalogue.Models;
 
 public class Player
 {
+    public int Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
 
